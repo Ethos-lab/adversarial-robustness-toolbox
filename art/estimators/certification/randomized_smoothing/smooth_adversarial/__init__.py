b2bad8cdd78192043467b6f19932e5e68d71b70e
Farhan Ahmed <Farhan.Ahmed@ibm.com>
2023-04-20 17:04:22 -0700

    refactor macer and smoothadv into randomized smoothing
    Signed-off-by: Farhan Ahmed <Farhan.Ahmed@ibm.com>

--- art/estimators/certification/randomized_smoothing/smooth_adversarial/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Smooth adversarial estimator
-"""
-from art.estimators.certification.randomized_smoothing.smooth_adversarial.train_smoothadv import fit_pytorch_smoothadv
-from art.estimators.certification.randomized_smoothing.smooth_adversarial.train_smoothadv import (
-    fit_tensorflow_smoothadv,
-)
